90b9dae0aef4f9592d7663c50e489e1af51274e0
Andy <andygnewman@gmail.com>
2015-02-06 15:44:55 +0000

    upto 12th question

--- lib/questions.rb
+++ lib/questions.rb
@@ -42,26 +42,31 @@ end
 # discard the first 3 elements of an array, 
 # e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
 def all_elements_except_first_3(array)
+  array.drop(3)
 end
 
 # add an element to the beginning of an array
 def add_element_to_beginning_of_array(array, element)
+  array.unshift(element)
 end
 
 # sort an array of words by their last letter, e.g.
 # ['sky', 'puma', 'maker'] becomes ['puma', 'maker', 'sky']
 def array_sort_by_last_letter_of_word(array)
+  array.map(&:reverse).sort.map(&:reverse)
 end
 
 # cut strings in half, and return the first half, e.g.
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
+  string[0,(string.length/2.to_f).ceil]
 end
 
 # turn a positive integer into a negative integer. A negative integer
 # stays negative
 def make_numbers_negative(number)
+  [number, 0-number].min {|a, b| a <=> b }
 end
 
 # turn an array of numbers into two arrays of numbers, one an array of 
@@ -69,6 +74,9 @@ end
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
 def separate_array_into_even_and_odd_numbers(array)
+  new_array = [[],[]]
+  array.each { |num| new_array[num % 2] << num  }
+  return new_array
 end
 
 # count the numbers of elements in an element which are palindromes
